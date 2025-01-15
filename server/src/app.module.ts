218461b9c81cd5f91c0fc9c11c55f5c7f0095c50
mzngu <eniyukuri97@gmail.com>
2025-01-15 16:34:50 +0100

    :sparkles:

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ChatGateway } from './chat/chat.gateway';
 import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, UsersModule],
   controllers: [AppController],
   providers: [AppService, ChatGateway],
 })
