4559231c7e2637a7007fd3041af3adbbe78600da
mzngu <eniyukuri97@gmail.com>
2025-01-22 22:43:11 +0100

    :zap:

--- irc-server/src/users/schemas/user.schema.ts
+++ irc-server/src/users/schemas/user.schema.ts
@@ -3,17 +3,22 @@ import { HydratedDocument, Types } from 'mongoose';
 
 export type UserDocument = HydratedDocument<User>;
 
-@Schema({ timestamps: true }) 
+@Schema({ timestamps: true })
 export class User {
+  @Prop({ type: Types.ObjectId }) 
+  _id: Types.ObjectId;
 
-  @Prop({ required: true, unique: true, minlength: 3 }) 
+  @Prop({ required: true, unique: true, minlength: 3 })
   username: string;
 
-  @Prop({ required: true, unique: true, match: /^[^\s@]+@[^\s@]+\.[^\s@]+$/ }) 
+  @Prop({ required: true, unique: true, match: /^[^\s@]+@[^\s@]+\.[^\s@]+$/ })
   email: string;
 
+  @Prop({ required: true, unique: true })
+  phoneNumber: string;
+
   @Prop({ required: true })
-  password: string;
+  passwordHash: string;
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
