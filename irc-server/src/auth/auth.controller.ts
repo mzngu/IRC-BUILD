b4f43da129322456c661e0cb8e1e57b6239e4073
mzngu <eniyukuri97@gmail.com>
2025-01-27 16:35:08 +0100

    auth controller and service implemented

---
+++ irc-server/src/auth/auth.controller.ts
@@ -0,0 +1,30 @@
+import { Controller, Post, Body, ValidationPipe, UsePipes, HttpCode, HttpStatus } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { LoginUserDto } from '../users/dto/login-user.dto'
+import { CreateUserDto } from '../users/dto/create-user.dto';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('register')
+  @UsePipes(new ValidationPipe())
+  async register(@Body() createUserDto: CreateUserDto) {
+    try {
+      return await this.authService.register(createUserDto);
+    } catch (error) {
+        throw error
+    }
+  }
+
+  @Post('login')
+  @HttpCode(HttpStatus.OK)
+  @UsePipes(new ValidationPipe())
+  async login(@Body() loginUserDto: LoginUserDto) {
+      try {
+          return await this.authService.login(loginUserDto)
+      } catch (error) {
+          throw error
+      }
+  }
+}
---
+++ irc-server/src/auth/auth.service.ts
@@ -0,0 +1,40 @@
+import { Injectable, UnauthorizedException, HttpException, HttpStatus } from '@nestjs/common';
+import { UsersService } from '../users/users.service';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
+import { LoginUserDto } from '../users/dto/login-user.dto'
+import { CreateUserDto } from '../users/dto/create-user.dto';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+  ) {}
+
+  async register(createUserDto: CreateUserDto) {
+      try {
+          return await this.usersService.createUser(createUserDto)
+      } catch (error) {
+          throw error
+      }
+  }
+
+  async login(loginUserDto: LoginUserDto) {
+    const user = await this.usersService.findOne(loginUserDto.username);
+    if (!user) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+
+    const isMatch = await bcrypt.compare(loginUserDto.password, user.passwordHash);
+
+    if (!isMatch) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+
+    const payload = { username: user.username, sub: user._id.toString() }; 
+    return {
+      access_token: await this.jwtService.signAsync(payload),
+    };
+  }
+}
