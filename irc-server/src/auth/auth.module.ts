c389e7015eb6599258d17071cf23cc3b09e797ad
mzngu <eniyukuri97@gmail.com>
2025-01-28 11:51:27 +0100

    test

--- irc-server/src/auth/auth.module.ts
+++ irc-server/src/auth/auth.module.ts
@@ -1,16 +1,17 @@
-import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
-import { LocalStrategy } from './strategies/local.strategy';
-import { AuthController } from './auth.controller';
+import { forwardRef, Module } from '@nestjs/common';
+import { UsersModule } from '../users/users.module'; 
 import { PassportModule } from '@nestjs/passport';
-import { UsersModule } from '../users/users.module';
 import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from './strategies/jwt.strategy';
+import { LocalStrategy } from './strategies/local.strategy';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+
 
 @Module({
   imports: [
+    forwardRef(() => UsersModule),
     PassportModule,
-    UsersModule,
     JwtModule.register({
       secret: process.env.JWT_SECRET || 'your_secret_key',
       signOptions: { expiresIn: '1h' },
--- irc-server/src/users/users.module.ts
+++ irc-server/src/users/users.module.ts
@@ -1,17 +1,15 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
+import { AuthModule } from '../auth/auth.module'; 
 import { MongooseModule } from '@nestjs/mongoose';
-import { UsersController } from './users.controller';
-import { UsersService } from './users.service';
 import { User, UserSchema } from './schemas/user.schema';
-import { AuthModule } from '../auth/auth.module';
+import { UsersService } from './users.service';
 
 @Module({
   imports: [
+    forwardRef(() => AuthModule),
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
-    AuthModule,
   ],
-  controllers: [UsersController],
   providers: [UsersService],
-  exports: [UsersService]
+  exports: [UsersService],
 })
 export class UsersModule {}
