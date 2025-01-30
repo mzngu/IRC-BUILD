6626e57c71f5f4b262e9e9dcca6dd1f72d062f1e
mzngu <eniyukuri97@gmail.com>
2025-01-30 22:08:20 +0100

    :zap:

--- client/src/components/SignUp.tsx
+++ client/src/components/SignUp.tsx
@@ -13,7 +13,7 @@ const Signup: React.FC = () => {
 
     try {
       const response = await axios.post('http://localhost:3000/auth/register', {
-        username, // Use the state values here
+        username,
         email,
         password,
       });
@@ -28,7 +28,7 @@ const Signup: React.FC = () => {
       }
 
       alert('Signup successful');
-      // Redirect or update state as needed (e.g., clear form fields)
+      
       setUsername('');
       setEmail('');
       setPassword('');
@@ -52,7 +52,7 @@ const Signup: React.FC = () => {
         <label className="block text-gray-700 text-sm font-bold mb-2">Username:</label>
         <input
           type="text"
-          value={username} // Use the state value here
+          value={username} 
           onChange={(e) => setUsername(e.target.value)}
           required
           className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
@@ -63,7 +63,7 @@ const Signup: React.FC = () => {
         <label className="block text-gray-700 text-sm font-bold mb-2">Email:</label>
         <input
           type="email"
-          value={email} // Use the state value here
+          value={email} 
           onChange={(e) => setEmail(e.target.value)}
           required
           className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
@@ -74,7 +74,7 @@ const Signup: React.FC = () => {
         <label className="block text-gray-700 text-sm font-bold mb-2">Password:</label>
         <input
           type="password"
-          value={password} // Use the state value here
+          value={password} 
           onChange={(e) => setPassword(e.target.value)}
           required
           className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
